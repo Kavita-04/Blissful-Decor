235e109b3c9f726dd364219ba6facd3472d7cb7a
Kavita-04 <88962376+Kavita-04@users.noreply.github.com>
2021-12-01 17:07:00 +0530

    Update login_submit.php

--- Blissful Decor/login_submit.php
+++ Blissful Decor/login_submit.php
@@ -10,21 +10,37 @@ if (isset($_SESSION['email'])) {
 $email = $_POST['e-mail'];
 $email = mysqli_real_escape_string($con, $email);
 $password = $_POST['password'];
-$password = mysqli_real_escape_string($con, $password);
+$pwd = mysqli_real_escape_string($con, $password);
+
 
 // Query checks if the email and password are present in the database.
-$query = "SELECT id,email FROM users WHERE email= '$email' AND password='$password'";
+// $query = "SELECT id,email FROM users WHERE email= '$email' AND password='$pwd'";
+$query = "SELECT id,email,password FROM users WHERE email= '$email'";
 $result = mysqli_query($con, $query)  or die(mysqli_error($con));
 $num = mysqli_num_rows($result);
 // If the email and password are not present in the database, the mysqli_num_rows returns 0, it is assigned to $num.
 if ($num == 0) {
 
-  $error = "<span class='red'>Please enter correct E-mail id and Password</span>";
+  $error ="<span class='red'>Please enter correct E-mail id</span>";
   header('location: login.php?error=' . $error);
-} else {  
+} else {
+
   $row = mysqli_fetch_array($result);
-  $_SESSION['email'] = $row['email'];
-  $_SESSION['user_id'] = $row['id'];
-  header('location: products.php');
+
+  if(base64_decode($row['password']) != $pwd)
+  {
+  	$error ="<span class='red'>Please enter correct Password</span>";
+  	header('location: login.php?error=' . $error);
+  }
+  else
+  {
+  	$_SESSION['email'] = $row['email'];
+  	$_SESSION['user_id'] = $row['id'];
+
+  	header('location: products.php');
+    
+
+  }
+  
 }
 ?>
