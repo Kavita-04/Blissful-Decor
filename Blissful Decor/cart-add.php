85279c235ca3e80f503d20d1fff21219224ff1a3
Kavita-04 <88962376+Kavita-04@users.noreply.github.com>
2021-12-01 16:58:21 +0530

    Update cart-add.php

--- Blissful Decor/cart-add.php
+++ Blissful Decor/cart-add.php
@@ -3,8 +3,22 @@ require("includes/common.php");
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $item_id = $_GET['id'];
     $user_id = $_SESSION['user_id'];
-    $query = "INSERT INTO users_items(user_id, item_id, status) VALUES('$user_id', '$item_id', 'Add to Cart')";
+
+
+    $q= "SELECT count(*) as count  from users_items where user_id='$user_id' AND item_id='$item_id' AND status='Add to Cart';";
+
+    $result=mysqli_query($con, $q)or die(mysqli_error($con));
+    $row = mysqli_fetch_array($result);
+    if ($row['count'] >= 1) {
+    	header('Location: ' . $_SERVER['HTTP_REFERER']."?add=1");
+    }
+    
+    else{
+    $query = "INSERT INTO users_items(user_id, item_id,status) VALUES('$user_id', '$item_id', 'Add to Cart')";
     mysqli_query($con, $query) or die(mysqli_error($con));
-    header('location: products.php');
+    header('Location: ' . $_SERVER['HTTP_REFERER']."?add=1");
+	}
+    
+     
 }
 ?>   
