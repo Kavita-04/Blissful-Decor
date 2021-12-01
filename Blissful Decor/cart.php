c7148ec22108d3cdd9f9c976ed2fecc10fe86ab0
Kavita-04 <88962376+Kavita-04@users.noreply.github.com>
2021-12-01 17:02:42 +0530

    Update cart.php

--- Blissful Decor/cart.php
+++ Blissful Decor/cart.php
@@ -8,59 +8,91 @@ if (!isset($_SESSION['email'])) {
 <html lang="en">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Cart | Blissful Decor </title>
+        <title>Cart | Blissful Decor</title>
         <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="css/myStyle.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
         
     </head>
     <body>
         <div class="container-fluid" id="content">
             <?php include 'includes/header.php'; ?>
             <div class="row decor_bg">
-                <div class="col-md-6 col-md-offset-3">
-                    <table class="table table-striped">
-    
+                <center><h2>Shopping Cart</h2> <hr>
+                <div id="cart"class="col-md-6 col-md-offset-3">
+                    <br>
                         <!--show table only if there are items added in the cart-->
                         <?php
-                        $sum = 0;
+                        $sum = 0.00;
                         $id="";
                         $user_id = $_SESSION['user_id'];
-                        $query = "SELECT items.price AS Price, items.id, items.name AS Name FROM users_items JOIN items ON users_items.item_id = items.id WHERE users_items.user_id='$user_id' and status='Add to Cart'";
+                        $query = "SELECT items.price AS Price,quantity,items.id , items.name AS Name FROM users_items JOIN items ON users_items.item_id = items.id WHERE users_items.user_id='$user_id' and status='Add to Cart'";
                         $result = mysqli_query($con, $query)or die($mysqli_error($con));
                         if (mysqli_num_rows($result) >= 1) {
                             ?>
-                            <thead>
+                            <table class="table" style="background-color: white">
+                            <thead style="background-color: #0EBC7F; color: white">
                                 <tr>
-                                    <th>Item Number</th>
-                                    <th>Item Name</th>
-                                    <th>Price</th>
                                     <th></th>
+                                    <th>Item Name</th>
+                                    <th>Quantity</th>
+                                    <th>Price (INR)</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
                                 while ($row = mysqli_fetch_array($result)) {
-                                    $sum+= $row["Price"];
-                                    $id .= $row["id"] . ", ";
-                                    echo "<tr><td>" . "#" . $row["id"] . "</td><td>" . $row["Name"] . "</td><td>Rs " . $row["Price"] . "</td>
-                                    <td>
-                                        <a href='cart-remove.php?id={$row['id']}' class='remove_item_link'> Remove</a>
-                                        </td>
-                                        </tr>";
+                                    if(isset($_POST['quan']) && $row["id"] == $_POST['id'] )
+                                    {
+                                        $quantity=$_POST['quan'];
+
+                                        mysqli_query($con,"UPDATE users_items SET quantity=$quantity WHERE user_id='".$user_id."' and  item_id='".$row['id']."' and status='Add to Cart' ");
+                                        header("location:cart.php");
+                                        unset($_POST['quan']);
+
+                                    }
+
+                                  
+                                    $quantity= $row["quantity"];
+                                    
+                                    $sum+= $row["Price"]*$quantity;
+                                    $id .= $row["id"] . ",";
+                                    // $_POST['id']=$row['id'];
+                                    
+
+                                    echo "<tr><td> <a href='cart-remove.php?id={$row['id']}'> <b class=' btn-danger glyphicon  glyphicon-minus-sign'></b></a></td>"."<td>"  . $row["Name"] . "</td><td> <form action='' method='POST'><input type='hidden' name='id' value='".$row['id']."'><input type='number' id='quantity' name='quan' value='".$quantity."' min='1' step='1'/></form> "."</td><td> " . $row["Price"] . "</td></tr>";
+
+                                    
+                                   
                                 }
                                 $id = rtrim($id, ", ");
-                                echo "<tr><td></td><td>Total</td><td>Rs " . $sum . "</td><td><a href='success.php?itemsid=" . $id . "' class='btn btn-primary'>Confirm</a></td></tr>";
+                                
+                                
                                 ?>
                             </tbody>
+
+
+                            <table class="table" style="margin-bottom: 0px; padding-bottom: 0px" >
+                                <tr>
+                                    <td>
+                                        <b class="btn btn-block btn-success">Bill Amount : <?php echo $sum;?> INR</b>
+                                    </td>
+                                    <td>
+                                        <a href="success.php?itemsid=<?php echo $id; ?>" class="btn btn-block btn-primary" ><b class="glyphicon  glyphicon-shopping-cart"></b> Checkout</a>
+                                    </td>
+                                </tr>
+                            </table>
                             <?php
-                        } else {
-                            echo "Please add items to the cart first!";
+                        } 
+                        else {
+                            echo "<b>Please add items to the cart first!</b><br>"; 
                         }
                         ?>
-                        <?php
-                        ?>
-                    </table>
+                        <br>
+
                 </div>
+            </center>
             </div>
         </div>
         <?php include("includes/footer.php"); ?>
