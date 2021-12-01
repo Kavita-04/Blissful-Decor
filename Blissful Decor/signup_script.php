6a2d650ead07f63615c0344d5e69e5dba3b3a13d
Kavita-04 <88962376+Kavita-04@users.noreply.github.com>
2021-12-01 17:14:31 +0530

    Update signup_script.php

--- Blissful Decor/signup_script.php
+++ Blissful Decor/signup_script.php
@@ -10,14 +10,12 @@ require("includes/common.php");
   $email = mysqli_real_escape_string($con, $email);
 
   $password = $_POST['password'];
-  $password = mysqli_real_escape_string($con, $password);
+  $pwd=base64_encode($password);
+  $pwd = mysqli_real_escape_string($con, $pwd);
 
 
   $contact = $_POST['contact'];
   $contact = mysqli_real_escape_string($con, $contact);
-
-  $city = $_POST['city'];
-  $city = mysqli_real_escape_string($con, $city);
 
   $address = $_POST['address'];
   $address = mysqli_real_escape_string($con, $address);
@@ -46,11 +44,11 @@ require("includes/common.php");
     header('location: signup.php?m3=' . $m3);
   } else {
     
-    $query = "INSERT INTO users(name, email, password, contact, city, address)VALUES('" . $name . "','" . $email . "','" . $password . "','" . $contact . "','" . $city . "','" . $address . "')";
+    $query = "INSERT INTO users(name, email, password, contact_no, address)VALUES('" . $name . "','" . $email . "','" . $pwd . "','" . $contact .  "','" . $address . "')";
     mysqli_query($con, $query) or die(mysqli_error($con));
     $user_id = mysqli_insert_id($con);
     $_SESSION['email'] = $email;
     $_SESSION['user_id'] = $user_id;
-    header('location: products.php');
+    header('location: index.php');
   }
 ?>
