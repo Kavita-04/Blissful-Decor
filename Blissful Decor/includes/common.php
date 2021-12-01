2660b601e64b614ce2300ccf79d2fe3f9046eee8
Sakshi Sharma <47790632+SakshiiSharmaa@users.noreply.github.com>
2021-12-01 16:40:21 +0530

    Uploaded common files

---
+++ Blissful Decor/includes/common.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = '12345';
+$db_name = "BlissfulDecor";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $db_name);
+// Check connection
+if (!$con) {
+ die("Connection failed: " . mysqli_connect_error());
+}
+if(session_id() == '')
+session_start();
+?>
