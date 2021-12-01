ccd3399f74116c53475e45ff8e618abd01ce6467
Kavita-04 <88962376+Kavita-04@users.noreply.github.com>
2021-12-01 17:15:13 +0530

    Update success.php

--- Blissful Decor/success.php
+++ Blissful Decor/success.php
@@ -5,20 +5,23 @@ if (!isset($_SESSION['email'])) {
     header('location: index.php');
 }
 $user_id = $_SESSION['user_id'];
-$item_ids_string = $_GET['itemsid'];
+$item_ids_string = $_GET['itemsid'] ;
 
 //We will change the status of the items purchased by the user to 'Confirmed'
-$query = "UPDATE users_items SET status='Confirmed' WHERE user_id=" . $user_id . " AND item_id IN (" . $item_ids_string . ") and status='Add to cart'";
-mysqli_query($con, $query) or die($mysqli_error($con));
+
+$query = "UPDATE users_items SET status='Confirmed' WHERE user_id=". $user_id ." AND item_id IN (". $item_ids_string .")AND status='Add to Cart'";
+mysqli_query($con, $query) or die(mysqli_error($con));
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta name="viewport" content="width = device-width, initial-scale = 1">
-        <title>Success | Blissful Decor </title>
+        <title>Success | Blissful Decor</title>
         <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="css/myStyle.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php include 'includes/header.php'; ?>
